b7ac4f928adf4553a29f0dcbcf78f0df764d517f
Legend_7ok <dimtkac777@gmail.com>
2026-05-15 18:44:48 +0300

    feat: add database connection

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import app from './src/app.js';
+import connectDB from "./src/db/connect.js";
 
 
 const PORT = process.env.PORT || 3000;
 
+await connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
