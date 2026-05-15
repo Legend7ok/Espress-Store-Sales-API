5d260397ee80791f3c5c88625f4fcb35e941c460
Legend_7ok <dimtkac777@gmail.com>
2026-05-15 14:30:16 +0300

    chore: add base status codes into constants.js

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const STATUS = {
+    ACTIVE: 1,
+    INACTIVE: 0,
+};
+
+export const HTTP = {
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+    SERVER_ERROR: 500,
+};
